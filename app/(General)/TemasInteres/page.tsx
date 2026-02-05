780f7ed722b9f54657d69680870341ed6c0b669a
Kevin Mendoza <ksmv233@gmail.com>
2026-02-05 03:41:00 -0600

    Corregir error bootstrap falta en layout.tsx. Cambios al layout para encapsular el provider y poder mantener el estado de favoritos de forma global y Cambiar las pages con respecto al cambio en layout, sin encapsular el TopicsProvider en ellos

--- app/(General)/TemasInteres/page.tsx
+++ app/(General)/TemasInteres/page.tsx
@@ -3,27 +3,20 @@
 import React from 'react';
 import TopicList from '../../Components/TopicList';
 import Link from 'next/link';
-import { TopicsProvider } from '../../Providers/TopicsProvider';
 
 export default function TemasInteres() {
   return (
-    <TopicsProvider>
-      <section>
-        <div className="d-flex justify-content-between align-items-center mb-3">
-          <div>
-            <h1 className="h4">Temas Favoritos</h1>
-            <p className="text-muted mb-0">
-              Estos son tus temas favoritos
-            </p>
-          </div>
-          <div>
-            <Link href="/" className="btn btn-outline-secondary">
-              Volver
-            </Link>
-          </div>
+    <section>
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <div>
+          <h1 className="h4">Temas Favoritos</h1>
         </div>
-        <TopicList onlyInteresantes />
-      </section>
-    </TopicsProvider>
+        <div>
+          <Link href="/" className="btn btn-outline-secondary">Volver al inicio</Link>
+        </div>
+      </div>
+
+      <TopicList onlyInteresantes />
+    </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,22 @@
 import React from 'react';
 import TopicList from './Components/TopicList';
 import Link from 'next/link';
-import { TopicsProvider } from './Providers/TopicsProvider';
 
 export default function Page() {
   return (
-    <TopicsProvider>
-      <section>
-        <div className="d-flex justify-content-between align-items-center mb-3">
-          <div>
-            <h1 className="h4">Temas Disponibles</h1>
-          </div>
-          <div>
-            <Link href="/TemasInteres" className="btn btn-primary">
-              Ver Favoritos
-            </Link>
-          </div>
+    <section>
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <div>
+          <h1 className="h4">Temas Disponibles</h1>
         </div>
+        <div>
+          <Link href="/TemasInteres" className="btn btn-primary">
+            Ver Favoritos
+          </Link>
+        </div>
+      </div>
 
-        <TopicList />
-      </section>
-    </TopicsProvider>
+      <TopicList />
+    </section>
   );
 }
